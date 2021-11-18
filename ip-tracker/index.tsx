aaaf4d9f0f105098e7b39d01b17d2098d6d1e0e0
Arya Tathe <53153924+excalibur1702@users.noreply.github.com>
2021-11-18 13:16:36 +0530

    Delete ip-tracker directory

--- ip-tracker/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
-
-import "@fontsource/rubik/400.css";
-import "@fontsource/rubik/500.css";
-import "@fontsource/rubik/700.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
